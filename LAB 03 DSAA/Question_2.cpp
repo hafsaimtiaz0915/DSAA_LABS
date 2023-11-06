3a90cffe67d157ac3bb0adb7d341e605c93ea94e
HAFSA IMTIAZ <129318751+hafsaimtiaz0915@users.noreply.github.com>
2023-11-06 22:13:31 +0500

    Update Question_2.cpp 06/NOV/2023

--- LAB 03 DSAA/Question_2.cpp
+++ LAB 03 DSAA/Question_2.cpp
@@ -2,53 +2,43 @@
 #include <vector>
 using namespace std;
 
-class Node
-{
+class Node {
 public:
     char data;
-    Node *next;
+    Node* next;
 
-    Node(char data)
-    {
+    Node(char data) {
         this->data = data;
         this->next = NULL;
     }
 };
 
-class SinglyLinkedList
-{
+class SinglyLinkedList {
 public:
-    Node *head;
+    Node* head;
 
-    SinglyLinkedList()
-    {
+    SinglyLinkedList() {
         head = NULL;
     }
 
-    void appendNode(char data)
-    {
-        Node *newNode = new Node(data);
+    void appendNode(char data) {
+        Node* newNode = new Node(data);
 
-        if (head == NULL)
-        {
+        if (head == NULL) {
             head = newNode;
         }
-        else
-        {
-            Node *ptr = head;
-            while (ptr->next != NULL)
-            {
+        else {
+            Node* ptr = head;
+            while (ptr->next != NULL) {
                 ptr = ptr->next;
             }
             ptr->next = newNode;
         }
     }
 
-    void displayList()
-    {
-        Node *temp = head;
-        while (temp != NULL)
-        {
+    void displayList() {
+        Node* temp = head;
+        while (temp != NULL) {
             cout << temp->data << " -> ";
             temp = temp->next;
         }
@@ -56,24 +46,17 @@ public:
     }
 };
 
-void swapLinkedList(string word, SinglyLinkedList &list, int stringLength)
-{
-    Node *current = list.head;
-    Node *position = list.head;
+void swapLinkedList(string word, SinglyLinkedList& list, int stringLength) {
+    Node* current = list.head;
+    Node* position = list.head;
 
-    for (int i = 0; i < stringLength; i++)
-    {
-        while (current != NULL)
-        {
-            if (word[i] == current->data)
-            {
-                if (current == position)
-                {
+    for (int i = 0; i < stringLength; i++) {
+        while (current != NULL) {
+            if (word[i] == current->data) {
+                if (current == position) {
                     position = position->next;
                     break;
-                }
-                else
-                {
+                } else {
                     char temp = position->data;
                     position->data = current->data;
                     current->data = temp;
@@ -88,39 +71,28 @@ void swapLinkedList(string word, SinglyLinkedList &list, int stringLength)
     list.displayList();
 }
 
-void checkDictionary(const vector<string> &dictionary, SinglyLinkedList &list, int stringLength)
-{
-    Node *current = list.head;
+void checkDictionary(const vector<string>& dictionary, SinglyLinkedList& list, int stringLength) {
+    Node* current = list.head;
     int count = 0;
 
-    for (int i = 0; i < dictionary.size(); i++)
-    {
+    for (int i = 0; i < dictionary.size(); i++) {
         string word = dictionary[i];
-        if (word.length() == stringLength)
-        {
-            for (int j = 0; j < stringLength; j++)
-            {
+        if (word.length() == stringLength) {
+            for (int j = 0; j < stringLength; j++) {
                 current = list.head;
-                while (current != NULL)
-                {
-                    if (word[j] == current->data)
-                    {
+                while (current != NULL) {
+                    if (word[j] == current->data) {
                         count++;
                         break;
-                    }
-                    else
-                    {
+                    } else {
                         current = current->next;
                     }
                 }
             }
-            if (count == stringLength)
-            {
+            if (count == stringLength) {
                 swapLinkedList(word, list, stringLength);
                 return;
-            }
-            else
-            {
+            } else {
                 count = 0;
             }
         }
@@ -128,11 +100,10 @@ void checkDictionary(const vector<string> &dictionary, SinglyLinkedList &list, i
     cout << "Word not found in dictionary" << endl;
 }
 
-int main()
-{
+int main() {
     SinglyLinkedList list;
     vector<string> dictionary = {
-        // 3-Letter Words
+         // 3-Letter Words
         "cat", "dog", "run", "sun", "eat", "hat", "pen", "top", "pig", "box",
         "car", "bus", "bed", "cup", "map", "key", "leg", "egg", "ant", "van",
         "toy", "arm", "win", "fan", "lip", "cap", "job", "nap", "fox", "rag",
@@ -144,7 +115,7 @@ int main()
         "car", "fan", "job", "bus", "bed", "cup", "toy", "pig", "dog", "cat",
         "fox", "nap", "hat", "leg", "eat", "pie", "hat", "top", "ice", "rug",
 
-        // 4-Letter Words
+        //4-Letter Words
         "bird", "jump", "play", "desk", "moon", "book", "fire", "rose", "blue", "song",
         "star", "lake", "ring", "pear", "fish", "lamp", "tree", "frog", "fish", "bear",
         "kite", "boat", "duck", "coat", "ball", "goat", "barn", "drum", "gate", "nose",
@@ -156,35 +127,25 @@ int main()
         "rain", "rice", "wolf", "bone", "hand", "flag", "leaf", "cake", "shoe", "gate",
         "pine", "duck", "book", "moon", "kite", "corn", "barn", "goat", "ball", "goat",
 
-        // 5-Letter Words
-        "apple", "water", "table", "snake", "queen", "chair", "ocean", "radio", "beach", "horse",
-        "music", "plant", "whale", "tiger", "paper", "fence", "smile", "clock", "peace", "lunch",
-        "bread", "eagle", "dream", "peach", "child", "party", "river", "lemon", "socks", "stone",
-        "stars", "green", "books", "night", "candy", "grape", "watch", "mouse", "north", "socks",
-        "snake", "radio", "horse", "tiger", "table", "ocean", "queen", "chair", "apple", "plant",
-        "clock", "paper", "water", "smile", "bread", "child", "peace", "fence", "night", "lemon",
-        "beach", "lunch", "eagle", "dream", "candy", "river", "music", "peach", "party", "books",
-        "mouse", "green", "watch", "stars", "north"};
+       
+    };
 
     string userInput;
-    cout << "Enter a string with a length > 2 and < 6 letters: ";
+    cout << "Enter a word(string) with a length > 2 and < 5 letters: ";
     cin >> userInput;
 
-    if (userInput.length() > 2 && userInput.length() < 6)
-    {
-        for (int i = 0; i < userInput.length(); i++)
-        {
+    if (userInput.length() > 2 && userInput.length() < 5) {
+        for (int i = 0; i < userInput.length(); i++) {
             list.appendNode(userInput[i]);
         }
-    }
-    else
-    {
-        cout << "Requirements not met. Input string must be 3 to 5 letters long." << endl;
+    } else {
+        cout << "Requirements not met. Input string must be 3 to 4 letters long." << endl;
         return 0;
     }
-
+     cout << "\n=======================================================\n" << endl;
     cout << "Your input: ";
     list.displayList();
+     cout << "\n=======================================================\n" << endl;
     cout << "Checking the dictionary..." << endl;
     checkDictionary(dictionary, list, userInput.length());
 
